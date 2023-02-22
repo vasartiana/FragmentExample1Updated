4f54e55982a0fd42730f6b2ee3bc01031fe6aea4
vasartiana <vas.artiana1003@mail.ugm.ac.id>
2023-02-22 14:47:43 +0700

    second commit

---
+++ app/src/main/java/com/example/fragmentexample1updated/SimpleFragment.java
@@ -0,0 +1,62 @@
+package com.example.fragmentexample1updated;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+
+
+public class SimpleFragment extends Fragment {
+    private static final int YES = 0;
+    private static final int NO = 1;
+
+    public SimpleFragment(){
+
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_simple, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        final RadioGroup radioGroup = view.findViewById(R.id.radio_group);
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+                View radioButton = radioGroup.findViewById(i);
+                int index = radioGroup.indexOfChild(radioButton);
+                TextView textView = view.findViewById(R.id.fragment_header);
+
+                switch (index) {
+                    case YES:
+                        textView.setText(R.string.yes_message);
+                        break;
+                    case NO:
+                        textView.setText(R.string.no_message);
+                        break;
+                    default:
+                        break;
+                }
+            }
+        });
+        }
+    }
